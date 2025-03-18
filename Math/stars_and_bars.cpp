522134f0889ed00af8fcb4f6ba05cfdd6ba0ab4f
Piyash Basak <145838527+piyashbasak9@users.noreply.github.com>
2025-03-18 12:28:00 +0600

    Create stars_and_bars.cpp

---
+++ Math/stars_and_bars.cpp
@@ -0,0 +1,9 @@
+//lets x+y+z = 2, and the qus is how many way to make equation valid. here (= 2) is star and (+) is bar.
+
+ll ncr(int n, int r){
+    return fact[n] * ifact[n - r] % mod * ifact[r] % mod;
+}
+
+ll stars_and_bars(int stars, int bars){
+    return ncr(stars + bars, stars);
+}
