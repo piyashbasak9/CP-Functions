77ac1e758efd625cccbab21c9c95eab545c83d6c
Piyash Basak <145838527+piyashbasak9@users.noreply.github.com>
2025-07-10 10:19:48 +0600

    Update Ordered_set.cpp

--- PBDS/Ordered_set.cpp
+++ PBDS/Ordered_set.cpp
@@ -20,3 +20,16 @@ int main() {
   cout << mp.order_of_key(2) << endl; ///number of first elements less than k
   return 0;
 }
+
+
+
+// Ordered Multiset
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <typename T> using indexed_set = tree<T, null_type, less_equal <T>, rb_tree_tag, tree_order_statistics_node_update>;
+// order_of_key (k) : Number of items strictly smalller than k .
+// find_by_order(k) : K-th element in a set (counting from zero).
+
+indexed_set<int> st;
