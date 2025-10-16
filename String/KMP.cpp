921758e497a0991b1d06d618d055a22439fa4e10
Piyash Basak <145838527+piyashbasak9@users.noreply.github.com>
2025-10-16 17:53:09 +0600

    Update KMP.cpp

--- String/KMP.cpp
+++ String/KMP.cpp
@@ -1,49 +1,29 @@
-void compute(string pat, int m , vector<int>&lps){
-        int len = 0, i = 1;
-        lps[0] = 0;
-        while(i < m){
-            if(pat[i] == pat[len]){
-                lps[i] = len + 1;
-                len++,i++;
-            }
-            else{
-                if(len != 0){
-                    len = lps[len - 1];
-                }
-                else{
-                    lps[i] = 0;
-                    i++;
-                }
-            }
-        }
+vector<int> lps(string $t){
+	int m = s.length();
+	vector<int> pi(m);
+	for (int i = 1, j = 0; i < m; ++i) {
+    while (j > 0 and t[i] != t[j]) {
+      j = pi[j - 1];
     }
-  
-  
-  
-  
-    vector<int> search(string& pat, string& txt) {
-        int n = txt.length();
-        int m = pat.length();
-        vector<int>ans;
-        vector<int>lps(m+1,0);
-        compute(pat,m,lps);
-        int i = 0, j = 0;
-        while(i < n){
-            if(txt[i] == pat[j]){
-                i++, j++;
-            }
-            else{
-                if(j != 0){
-                    j = lps[j-1];
-                }
-                else{
-                    i++;
-                }
-            }
-            if(j == m){
-                ans.push_back(i - m);
-                j = lps[j - 1];
-            }
-        }
-        return ans;
+    if (t[i] == t[j]) ++j;
+    pi[i] = j;
+  }
+  return pi;
+}
+
+
+int kmp(string &s, string &t){
+	auto pi = lps(t);
+	int ans = 0;
+  for (int i = 0, j = 0; i < n; ++i) {
+    while(j > 0 and s[i] != t[j]) {
+      j = pi[j - 1];
     }
+    if (s[i] == t[j]) ++j;
+    if (j == m) {
+      ++ans;
+      j = pi[j - 1];
+    }
+  }
+  return ans;
+}
